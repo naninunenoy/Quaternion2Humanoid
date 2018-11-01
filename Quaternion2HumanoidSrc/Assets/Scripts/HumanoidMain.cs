579fcf28d64f1dd968629a8b9168eba94b9f63a2
Yosuke Nakano <ynakano@moff.mobi>
2018-11-01 20:37:15 +0900

    add humanoid scene

---
+++ Quaternion2HumanoidSrc/Assets/Scripts/HumanoidMain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Assets.Quaternion2Humanoid.Scripts {
+    public class HumanoidMain : MonoBehaviour {
+        [Header("* leftUpperArm")]
+        [SerializeField] Transform leftUpperArmTransform;
+        [SerializeField] UIReactiveQuaternion leftUpperArmReactiveQuaternion;
+        [Header("* leftForeArm")]
+        [SerializeField] Transform leftForeArmTransform;
+        [SerializeField] UIReactiveQuaternion leftForeArmReactiveQuaternion;
+
+        void Awake() {
+            // leftUpperArm
+            leftUpperArmReactiveQuaternion.OverrideQuaternion(leftUpperArmTransform.rotation);
+            leftUpperArmReactiveQuaternion.ReactiveQuaternion.Subscribe(
+                q => { leftUpperArmTransform.rotation = q; }
+            );
+            // leftForeArm
+            leftForeArmReactiveQuaternion.OverrideQuaternion(leftForeArmTransform.rotation);
+            leftForeArmReactiveQuaternion.ReactiveQuaternion.Subscribe(
+                q => { leftForeArmTransform.rotation = q; }
+            );
+        }
+    }
+}
