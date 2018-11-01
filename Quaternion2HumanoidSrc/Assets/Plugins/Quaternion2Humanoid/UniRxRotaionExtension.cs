331f27ff3a2ec6cae25c8970a901918103485ecb
nakano yosuke <naninuneno0411@gmail.com>
2018-11-02 00:39:54 +0900

    SubscribeToRotation

---
+++ Quaternion2HumanoidSrc/Assets/Plugins/Quaternion2Humanoid/UniRxRotaionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace UniRx {
+    public static class UniRxRotaionExtension {
+        public static IDisposable SubscribeToRotation(this IObservable<Quaternion> source, Transform transform) {
+            return source.SubscribeWithState(transform, (x, t) => t.rotation = x);
+        }
+        public static IDisposable SubscribeToLocalRotation(this IObservable<Quaternion> source, Transform transform) {
+            return source.SubscribeWithState(transform, (x, t) => t.rotation = x);
+        }
+    }
+}
