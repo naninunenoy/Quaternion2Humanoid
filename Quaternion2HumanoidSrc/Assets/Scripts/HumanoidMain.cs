ec6105baa598ac1cc5604c8c8303f7fe13369acc
nakano yosuke <naninuneno0411@gmail.com>
2018-11-01 23:50:11 +0900

    cr

--- Quaternion2HumanoidSrc/Assets/Scripts/HumanoidMain.cs
+++ Quaternion2HumanoidSrc/Assets/Scripts/HumanoidMain.cs
@@ -1,31 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UniRx;
-
-namespace Assets.Quaternion2Humanoid.Scripts {
-    public class HumanoidMain : MonoBehaviour {
-        [SerializeField] Animator humanoidAnimator;
-        [SerializeField] UIReactiveQuaternion leftUpperArmReactiveQuaternion;
-        [SerializeField] UIReactiveQuaternion leftLowerArmReactiveQuaternion;
-
-        void Awake() {
-            // leftUpperArm
-            var leftUpperArmTransform = humanoidAnimator.GetBoneTransform(HumanBodyBones.LeftUpperArm);
-            leftUpperArmReactiveQuaternion.SetDefaultQuaternion(leftUpperArmTransform.rotation);
-            leftUpperArmReactiveQuaternion.ReactiveQuaternion.Subscribe(
-                q => { 
-                    leftUpperArmTransform.rotation = q;
-                }
-            );
-            // leftLowerArm
-            var leftLowerArmTransform = humanoidAnimator.GetBoneTransform(HumanBodyBones.LeftLowerArm);
-            leftLowerArmReactiveQuaternion.SetDefaultQuaternion(leftLowerArmTransform.rotation);
-            leftLowerArmReactiveQuaternion.ReactiveQuaternion.Subscribe(
-                q => {
-                    leftLowerArmTransform.rotation = q;
-                }
-            );
-        }
-    }
-}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UniRx;namespace Assets.Quaternion2Humanoid.Scripts {    public class HumanoidMain : MonoBehaviour {        [SerializeField] Animator humanoidAnimator;        [SerializeField] UIReactiveQuaternion leftUpperArmReactiveQuaternion;        [SerializeField] UIReactiveQuaternion leftLowerArmReactiveQuaternion;        void Awake() {            // leftUpperArm            var leftUpperArmTransform = humanoidAnimator.GetBoneTransform(HumanBodyBones.LeftUpperArm);            leftUpperArmReactiveQuaternion.SetDefaultQuaternion(leftUpperArmTransform.rotation);            leftUpperArmReactiveQuaternion.ReactiveQuaternion.Subscribe(                q => {                     leftUpperArmTransform.rotation = q;                }            );            // leftLowerArm            var leftLowerArmTransform = humanoidAnimator.GetBoneTransform(HumanBodyBones.LeftLowerArm);            leftLowerArmReactiveQuaternion.SetDefaultQuaternion(leftLowerArmTransform.rotation);            leftLowerArmReactiveQuaternion.ReactiveQuaternion.Subscribe(                q => {                    leftLowerArmTransform.rotation = q;                }            );        }    }}
