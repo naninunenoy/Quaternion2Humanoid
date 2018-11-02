e510451d417f0092a50bb805f53f7985ea718768
Yosuke Nakano <ynakano@moff.mobi>
2018-11-02 17:52:56 +0900

    add TransformRotaionExtension

---
+++ Quaternion2HumanoidSrc/Assets/Plugins/Quaternion2Humanoid/TransformRotaionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Quaternion2Humanoid {
+    public static class TransformRotaionExtension {
+        public static Quaternion GetRelativeQuaternion(this Transform baseTransform, Quaternion quaternion) {
+            var baseQuaternion = baseTransform.rotation;
+            return Quaternion.Inverse(baseQuaternion) * quaternion * baseQuaternion;
+        }
+        public static IObservable<Quaternion> ToRelativeQuaternionObservable(this IObservable<Quaternion> source, Transform baseTransform) {
+            return source.Select(baseTransform.GetRelativeQuaternion);
+        }
+    }
+}
+
