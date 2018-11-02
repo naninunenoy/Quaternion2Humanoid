884b215a96d8daa5384722179e07c9e0942d7d58
Yosuke Nakano <ynakano@moff.mobi>
2018-11-02 15:01:43 +0900

    シーン追加

---
+++ Quaternion2HumanoidSrc/Assets/Scripts/HumanoidBodyMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Quaternion2Humanoid.Scripts {
+    public class HumanoidBodyMain : MonoBehaviour {
+        [SerializeField] Button leftArmSceneButton;
+
+        void Start() {
+            // load scene event
+            leftArmSceneButton.onClick.AddListener(() => { SceneManager.LoadScene("LeftArmScene"); });
+        }
+    }
+}
--- Quaternion2HumanoidSrc/Assets/Scripts/LeftHandMain.cs
+++ Quaternion2HumanoidSrc/Assets/Scripts/LeftHandMain.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;using UnityEngine.SceneManagement;using UniRx;using Quaternion2Humanoid;namespace Assets.Quaternion2Humanoid.Scripts {    public class LeftHandMain : MonoBehaviour {        [SerializeField] ReactiveHumanoidBones humanoidBones;        [SerializeField] UIReactiveQuaternion leftUpperArmReactiveQuaternion;        [SerializeField] UIReactiveQuaternion leftLowerArmReactiveQuaternion;        [SerializeField] Button sampleSceneButton;        void Start() {
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;using UnityEngine.SceneManagement;using UniRx;using Quaternion2Humanoid;namespace Assets.Quaternion2Humanoid.Scripts {    public class LeftHandMain : MonoBehaviour {        [SerializeField] ReactiveHumanoidBones humanoidBones;        [SerializeField] UIReactiveQuaternion leftUpperArmReactiveQuaternion;        [SerializeField] UIReactiveQuaternion leftLowerArmReactiveQuaternion;        [SerializeField] Button sampleSceneButton;        [SerializeField] Button humanoidSceneButton;        void Start() {
             humanoidBones.SbscribeAsGlobalQuaternionTo(HumanBodyBones.LeftUpperArm, leftUpperArmReactiveQuaternion.ReactiveQuaternion).AddTo(this);
             humanoidBones.SbscribeAsGlobalQuaternionTo(HumanBodyBones.LeftLowerArm, leftLowerArmReactiveQuaternion.ReactiveQuaternion).AddTo(this);
             // observer
             humanoidBones.ReactiveBones.Subscribe(                bone => {
                     Debug.LogFormat("{0} : q={1}", bone.Bone, bone.Quaternion);
                 }            ).AddTo(this);            // load scene event
-            sampleSceneButton.onClick.AddListener(() => { SceneManager.LoadScene("SampleScene"); });        }    }}
+            sampleSceneButton.onClick.AddListener(() => { SceneManager.LoadScene("SampleScene"); });            humanoidSceneButton.onClick.AddListener(() => { SceneManager.LoadScene("HumanoidBodyScene"); });        }    }}
