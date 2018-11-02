3f9b5847b88d4d0b01e6d10f74d88a58be388b67
Yosuke Nakano <ynakano@moff.mobi>
2018-11-02 20:51:45 +0900

    add ChainedReactiveQuaternion

---
+++ Quaternion2HumanoidSrc/Assets/Plugins/Quaternion2Humanoid/IReactiveQuaternion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Quaternion2Humanoid {
+    public interface IOverwritableReactiveQuaternion : IReactiveQuaternion {
+        void OverwriteQuaternion(Quaternion quaternion);
+    }
+    public interface IReactiveQuaternion {
+        IReadOnlyReactiveProperty<Quaternion> ReactiveQuaternion { get; }
+    }
+
+    public class ChainedReactiveQuaternion : IReactiveQuaternion {
+        readonly IOverwritableReactiveQuaternion mine;
+
+        public ChainedReactiveQuaternion(IOverwritableReactiveQuaternion mine) { this.mine = mine; }
+
+        public void ChainToParent(IReactiveQuaternion parent) {
+            parent.ReactiveQuaternion.Subscribe(
+                parentQuat => {
+                    var q = mine.ReactiveQuaternion.Value;
+                    mine.OverwriteQuaternion(parentQuat * q);
+                }
+            );
+        }
+
+        public IReadOnlyReactiveProperty<Quaternion> ReactiveQuaternion { get { return mine.ReactiveQuaternion; } }
+    }
+}
